e861833a40f7c29515bb49e8b340e11aa40b9a56
JoeCory94 <joecory94@gmail.com>
2016-05-08 00:48:42 +0100

    2 views for home page

--- app/src/main/java/com/joe/payp/MainActivity.java
+++ app/src/main/java/com/joe/payp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.joe.payp;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
 
