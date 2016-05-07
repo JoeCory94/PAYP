f5eb1ec8c87d74a20624b1cd9ae8b7dfb3e34404
JoeCory94 <joecory94@gmail.com>
2016-05-08 00:35:58 +0100

    working on design - rounded button made

--- app/src/main/java/com/joe/payp/MainActivity.java
+++ app/src/main/java/com/joe/payp/MainActivity.java
@@ -5,17 +5,9 @@ import android.graphics.Typeface;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import org.joda.time.DateTime;
-import org.joda.time.LocalTime;
-import org.joda.time.Minutes;
-
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 /**
  * Created by Joe on 06/05/2016.
@@ -29,10 +21,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
 
-        TextView myTextView=(TextView)findViewById(R.id.Title);
-        Typeface typeFace= Typeface.createFromAsset(getAssets(),"fonts/OpenSans.ttf");
+        /*TextView myTextView=(TextView)findViewById(R.id.Title);
+        Typeface typeFace= Typeface.createFromAsset(getAssets(),"fonts/Roboto.ttf");
         myTextView.setTypeface(typeFace);
-
+*/
         DeviceID = Settings.Secure.getString(getApplicationContext().getContentResolver(),
                 Settings.Secure.ANDROID_ID);
 
